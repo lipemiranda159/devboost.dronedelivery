7c1715c327e9f71824fd9399d3884dbb86776b98
lipemiranda159 <lipemiranda159@hotmail.com>
2020-08-22 15:15:32 -0300

    create table PedidoDrone

---
+++ devboost.dronedelivery.felipe/devboost.dronedelivery.felipe/Models/PedidoDrone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devboost.dronedelivery.felipe.Models
+{
+    public class PedidoDrone
+    {
+        public int Id { get; set; }
+        public int DroneId { get; set; }
+        public int PedidoId { get; set; }
+        public DateTime DataHoraFinalizacao { get; set; }
+    }
+}
